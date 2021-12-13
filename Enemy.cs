74c344365ee5fd69346e350ba41b44c9039e2153
keinan01 <95875928+keinan01@users.noreply.github.com>
2021-12-13 12:09:25 -0600

    add colour

--- Enemy.cs
+++ Enemy.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -9,12 +9,14 @@ namespace Time_Pilot
 {
     class Enemy : Plane
     {
+        static Random rnd = new Random();
         public Enemy(Texture2D tex, Vector2 pos, float rad)
         {
             base.tex = tex;
             base.size = new Vector2(72, 72);
-            base.pos = pos;
+            base.pos = new Vector2(rnd.Next(0, 900), rnd.Next(0, 1000));
             base.rad = rad;
+            base.colour = new Color(rnd.Next(256), rnd.Next(256), rnd.Next(256));
         }
     }
 }
