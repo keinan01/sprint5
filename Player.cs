38a64e3e17f5763a2545c270b110e12fa979c93c
keinan01 <95875928+keinan01@users.noreply.github.com>
2021-12-13 11:37:38 -0600

    player class

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Time_Pilot
+{
+    class Player : Plane
+    {
+
+        public Player(Texture2D tex, Vector2 pos, float rad)
+        {
+            base.tex = tex;
+            base.size = new Vector2(72, 72);
+            base.pos = pos;
+            base.rad = rad;
+        }
+    }
+}
