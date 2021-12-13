b7af055ed958021fb7c991bdf86952879e36e8ef
keinan01 <95875928+keinan01@users.noreply.github.com>
2021-12-13 11:37:03 -0600

    Update Plane.cs

--- Plane.cs
+++ Plane.cs
@@ -1,11 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Diagnostics;
 
 namespace Time_Pilot
 {
     class Plane
     {
+        public Vector2 pos, vel, size, mPos;
+        public Texture2D tex;
+        public float rad;       
+        public Plane()
+        {
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 origin = new Vector2(tex.Bounds.Width / 2, tex.Bounds.Height / 2);
+            Debug.Print(origin.X + " " + origin.Y);
+            spriteBatch.Draw(tex, new Rectangle((int)pos.X, (int)pos.Y, (int)size.X, (int)size.Y), null, Color.White, rad, origin, SpriteEffects.None, 0f);
+        }
     }
 }
